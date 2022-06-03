c48f6206a7557cd7b9cc254da41be46daaf50c01
kamausuzan197 <kamaususan197@gmail.com>
2022-06-03 22:09:37 +0300

    complete task

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element  = document.querySelector("main")
+element.remove()
+
+// create newHeader
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+console.log(newHeader)
+newHeader.textContent = "susan is the champion" 
